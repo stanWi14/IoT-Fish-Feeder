a34c5cfd83588b1c3129004893b7e57b5f775dc6
Stanley Wisely <stan.w@partner.samsung.com>
2023-11-06 10:54:15 +0700

    Adding Schedule Recycler View Show & Delete

--- app/src/main/java/com/example/fishfeeder/EditActivity.kt
+++ app/src/main/java/com/example/fishfeeder/EditActivity.kt
@@ -6,6 +6,9 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ConcatAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.fishfeeder.databinding.ActivityEditBinding
 import com.example.fishfeeder.databinding.DialogAddScheduleBinding
 import com.example.fishfeeder.model.Device
@@ -16,13 +19,21 @@ class EditActivity : AppCompatActivity() {
     lateinit var deviceViewModel: DeviceViewModel
     lateinit var selectedDevice: Device
     val db = FirebaseFirestore.getInstance()
+    var monArray = mutableListOf<String>()
+    var tueArray = mutableListOf<String>()
+    var wedArray = mutableListOf<String>()
+    var thuArray = mutableListOf<String>()
+    var friArray = mutableListOf<String>()
+    var satArray = mutableListOf<String>()
+    var sunArray = mutableListOf<String>()
+    lateinit var devId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        val devId = intent.getStringExtra("DevId")
+        devId = intent.getStringExtra("DevId").toString()
         val devTitle = intent.getStringExtra("DevTitle")
         val devANotifVal = intent.getBooleanExtra("DevAN", false)
         val userStatus = intent.getBooleanExtra("UserStat", false)
@@ -56,21 +67,24 @@ class EditActivity : AppCompatActivity() {
                     }
             }
         }
+        if (devId != null) {
+            readData(devId)
+        }
         binding.btnAddSchedule.setOnClickListener() {
             if (devId != null) {
                 addScheduleDialog(devId)
+            }
+        }
+        binding.btnDisconnect.setOnClickListener() {
+            if (devId != null) {
+
             }
         }
 //        binding.sl
 
     }
 
-    fun readAllFeedingSchedule() {
-        // read all feeding schedule
-        // assign it into arrays
-    }
-
-    private fun addScheduleDialog(devId:String) {
+    private fun addScheduleDialog(devId: String) {
         val dialogViewBinding = DialogAddScheduleBinding.inflate(LayoutInflater.from(this))
         val dialogView = dialogViewBinding.root
 
@@ -101,6 +115,16 @@ class EditActivity : AppCompatActivity() {
 
             for ((day, checkBox) in dayToFieldMap) {
                 if (checkBox.isChecked) {
+                    // Add day to Local Array
+                    when (day) {
+                        "Monday" -> monArray.add(combinedString)
+                        "Tuesday" -> tueArray.add(combinedString)
+                        "Wednesday" -> wedArray.add(combinedString)
+                        "Thursday" -> thuArray.add(combinedString)
+                        "Friday" -> friArray.add(combinedString)
+                        "Saturday" -> satArray.add(combinedString)
+                        "Sunday" -> sunArray.add(combinedString)
+                    }
                     // Fetch the existing array from Firestore
                     db.collection("Schedules")
                         .document(devId)
@@ -136,19 +160,69 @@ class EditActivity : AppCompatActivity() {
                         }
                 }
             }
-
             alertDialog.dismiss()
+            showAllSchedule()
         }
         alertDialog.show()
     }
 
+    fun readData(devId: String) {
+        // Get data from Firebase
+        db.collection("Schedules")
+            .document(devId) // Specify the document ID
+            .get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val document = task.result
+                    if (document != null && document.exists()) {
+                        monArray = document.get("Monday") as MutableList<String>
+                        tueArray = document.get("Tuesday") as MutableList<String>
+                        wedArray = document.get("Wednesday") as MutableList<String>
+                        thuArray = document.get("Thursday") as MutableList<String>
+                        friArray = document.get("Friday") as MutableList<String>
+                        satArray = document.get("Saturday") as MutableList<String>
+                        sunArray = document.get("Sunday") as MutableList<String>
+                        showAllSchedule()
+                    } else {
+                        // Handle the case where the document with the specified ID doesn't exist
+                    }
+                } else {
+                    // Handle errors, e.g., network issues or Firestore security rules violation
+//                    binding.txtListSchedule.setText("Failed to read data: ${task.exception?.message}")
+                }
+            }
+    }
+
+
+    fun showAllSchedule() {
+        val recyclerView: RecyclerView = binding.rvSchedule
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // Assuming you have fetched data into your arrays
+        val monAdapter = ScheduleAdapter(monArray, this, devId, "Monday")
+        val tueAdapter = ScheduleAdapter(tueArray, this, devId, "Tuesday")
+        val wedAdapter = ScheduleAdapter(wedArray, this, devId, "Wednesday")
+        val thuAdapter = ScheduleAdapter(thuArray, this, devId, "Thursday")
+        val friAdapter = ScheduleAdapter(friArray, this, devId, "Friday")
+        val satAdapter = ScheduleAdapter(satArray, this, devId, "Saturday")
+        val sunAdapter = ScheduleAdapter(sunArray, this, devId, "Sunday")
+
+        val adapters = listOf(
+            monAdapter,
+            tueAdapter,
+            wedAdapter,
+            thuAdapter,
+            friAdapter,
+            satAdapter,
+            sunAdapter
+        )
+
+        val mergedAdapter = ConcatAdapter(adapters)
+        recyclerView.adapter = mergedAdapter
+    }
 
     fun deleteSchedule() {
         // remove an array value by button & recyclerview ?
         // duno how to implement rn
-    }
-
-    fun updateFireStore() {
-        // update schedule to firestore using now array
     }
 }
---
+++ app/src/main/java/com/example/fishfeeder/ScheduleAdapter.kt
@@ -0,0 +1,86 @@
+package com.example.fishfeeder
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
+
+class ScheduleAdapter(
+    private val data: List<String>,
+    private val activity: EditActivity,
+    private val devId: String,
+    private val dayOfWeek: String
+) : RecyclerView.Adapter<ScheduleAdapter.ScheduleViewHolder>() {
+
+    class ScheduleViewHolder(scheduleView: View) : RecyclerView.ViewHolder(scheduleView) {
+        val labelDay: TextView = scheduleView.findViewById(R.id.txtDay)
+        val labelTime: TextView = scheduleView.findViewById(R.id.txtTime)
+        val labelPortion: TextView = scheduleView.findViewById(R.id.txtPortion)
+        val btnDelSchedule: Button = scheduleView.findViewById(R.id.btnDelSchedule)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ScheduleAdapter.ScheduleViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_schedule_card, parent, false)
+        return ScheduleAdapter.ScheduleViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ScheduleViewHolder, position: Int) {
+        val currentFormat = data[position]
+        val hour = currentFormat.substring(0, 2).toInt()
+        val minute = if (currentFormat.length >= 4) currentFormat.substring(2, 4).toInt() else 0
+        val portion = if (currentFormat.length > 4) currentFormat.substring(4) else ""
+
+        holder.labelTime.text = "$hour:$minute"
+        holder.labelPortion.text = portion
+        holder.labelDay.text = dayOfWeek
+
+        holder.btnDelSchedule.setOnClickListener() {
+            deleteSchedule(currentFormat)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    fun deleteSchedule(deleteFormat: String) {
+        val db = FirebaseFirestore.getInstance()
+        val documentPath = "Schedules/$devId"
+        val dayToRemoveValueFrom = dayOfWeek
+        val valueToRemove = deleteFormat
+        val docRef = db.document(documentPath)
+
+        docRef.get().addOnSuccessListener { documentSnapshot ->
+            if (documentSnapshot.exists()) {
+                val data = documentSnapshot.data
+                if (data != null && data.containsKey(dayToRemoveValueFrom)) {
+                    val dayArray =
+                        data[dayToRemoveValueFrom] as ArrayList<String> // Use ArrayList<String>
+
+                    // Remove the specific value from the array
+                    dayArray.remove(valueToRemove)
+
+                    // Create a new data map with the updated array
+                    val updatedData = HashMap<String, Any>()
+                    updatedData[dayToRemoveValueFrom] = dayArray
+
+                    // Update the Firestore document with the modified data
+                    docRef.update(updatedData).addOnSuccessListener {
+                        // The value has been removed successfully
+                        activity.readData(devId)
+                    }.addOnFailureListener { e ->
+                        // Handle any errors that occurred during the update
+                    }
+                }
+            }
+        }
+    }
+
+}
